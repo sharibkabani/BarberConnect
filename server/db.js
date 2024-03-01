74eeeb732779383f7c0b281eded6b4b63c54fff3
Sharib Kabani <ksharib05@gmail.com>
2024-03-01 01:56:28 -0500

    Revert "ready to deploy"
    This reverts commit 635786934c5c4468ca527f7042fb1bd53ce250b0.

--- server/db.js
+++ server/db.js
@@ -1,15 +1,12 @@
-const Pool = require("pg").Pool;
-require("dotenv").config();
+const Pool = require('pg').Pool;
+require('dotenv').config();
 
 const pool = new Pool({
-	connectionString: process.env.POSTGRES_URL,
-});
-
-pool.connect((err, client, release) => {
-    if (err) {
-        return console.error("Error acquiring client", err.stack);
-    }
-    console.log("Connected to PostgreSQL");
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 });
 
 module.exports = pool;
