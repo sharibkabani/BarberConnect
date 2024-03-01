635786934c5c4468ca527f7042fb1bd53ce250b0
Sharib Kabani <ksharib05@gmail.com>
2024-03-01 00:11:14 -0500

    ready to deploy

--- server/db.js
+++ server/db.js
@@ -1,12 +1,15 @@
-const Pool = require('pg').Pool;
-require('dotenv').config();
+const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+	connectionString: process.env.POSTGRES_URL,
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error("Error acquiring client", err.stack);
+    }
+    console.log("Connected to PostgreSQL");
 });
 
 module.exports = pool;
